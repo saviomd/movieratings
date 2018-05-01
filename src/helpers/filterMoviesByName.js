f12b8d609b93c4c278b74d48b76fb1249f9db211
Sávio Mendes <saviomd@gmail.com>
2018-05-01 00:29:44 -0300

    Fix movie filter function and update dependencies

--- src/helpers/filterMoviesByName.js
+++ src/helpers/filterMoviesByName.js
@@ -1,6 +1,6 @@
 export default function filterMoviesByName (movieList, value) {
 	return movieList.filter((movie) => {
 		const movieName = movie.Name.toLowerCase();
-		return (movieName.includes(value));
+		return (movieName.includes(value.toLowerCase()));
 	});
 }
