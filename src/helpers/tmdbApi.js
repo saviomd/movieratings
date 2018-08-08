b62859a62f8a4c314a36fd772ae4f16ae5060838
Sávio Mendes <saviomd@gmail.com>
2018-08-07 23:33:45 -0300

    Update diary, ratings, dependencies and tmdb logo

--- src/helpers/tmdbApi.js
+++ src/helpers/tmdbApi.js
@@ -1,6 +1,6 @@
 export default {
 	img: {
-		attributionUrl: 'https://www.themoviedb.org/static_cache/v4/logos/408x161-powered-by-rectangle-green-bb4301c10ddc749b4e79463811a68afebeae66ef43d17bcfd8ff0e60ded7ce99.png',
+		attributionUrl: 'https://www.themoviedb.org/assets/1/v4/logos/408x161-powered-by-rectangle-green-bb4301c10ddc749b4e79463811a68afebeae66ef43d17bcfd8ff0e60ded7ce99.png',
 		baseUrl: 'https://image.tmdb.org/t/p/',
 		backdropSize: 'w1280',
 		fallbackUrl: 'https://www.themoviedb.org/static_cache/v4/logos/293x302-powered-by-square-blue-ee05c47ab249273a6f9f1dcafec63daba386ca30544567629deb1809395d8516.png',
