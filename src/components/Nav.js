a679df4aa60a1405ece2f726a7448aa8a11f1f92
Sávio Mendes <saviomd@gmail.com>
2018-08-30 14:00:36 -0300

    Add navitem component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,15 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
-const Nav = ({ navLinks }) => {
-	const htmlLinks = navLinks.map((link, index) => {
-		return (
-			<NavLink key={link.path} to={link.path} exact activeClassName="active" className="btn btn-danger btn-sm">
-				<FontAwesomeIcon icon={link.icon} />
-				<span className="d-none d-sm-inline-block ml-1">{link.name}</span>
-			</NavLink>
-		)
-	});
-	return (
-		<nav className="btn-group">
-			{htmlLinks}
-		</nav>
-	)
-}
+import NavItem from './NavItem';
+
+const Nav = ({ navLinks }) => (
+	<nav className="btn-group">
+		{navLinks.map(link => (
+			<NavItem key={link.path} link={link} />
+		))}
+	</nav>
+);
 
 Nav.propTypes = {
 	navLinks: PropTypes.array.isRequired
---
+++ src/components/NavItem.js
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavItem = ({ link }) => (
+	<NavLink to={link.path} activeClassName="active" className="btn btn-danger btn-sm">
+		<FontAwesomeIcon icon={link.icon} />
+		<span className="d-none d-sm-inline-block ml-1">{link.name}</span>
+	</NavLink>
+);
+
+NavItem.propTypes = {
+	link: PropTypes.shape({
+		icon: PropTypes.string.isRequired,
+		name: PropTypes.string.isRequired,
+		path: PropTypes.string.isRequired
+	})
+};
+
+export default NavItem;
