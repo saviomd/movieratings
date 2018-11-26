b4a08211c84bda4627a75adbe526741ccfb8eabb
Sávio Mendes <saviomd@gmail.com>
2018-11-25 23:42:59 -0200

    Add storybook addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,8 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
-import { configure } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
+import { addDecorator, configure } from '@storybook/react';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,5 +14,14 @@ function loadStories() {
   require('./welcomeStory');
   req.keys().forEach(file => req(file));
 }
+
+addDecorator(
+  withInfo({
+    header: false,
+    inline: true,
+    source: true,
+  })
+);
+addDecorator(withKnobs);
 
 configure(loadStories, module);
--- src/components/ProgressBar/ProgressBar.stories.js
+++ src/components/ProgressBar/ProgressBar.stories.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { number } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 
 import MockWrapper from '../MockWrapper';
 import ProgressBar from './ProgressBar';
 
 storiesOf('ProgressBar', module)
-  .add('50', () => (
+  .add('default', () => (
     <MockWrapper>
-      <ProgressBar width="50" />
-    </MockWrapper>
-  ))
-  .add('70', () => (
-    <MockWrapper>
-      <ProgressBar width="70" />
+      <ProgressBar width={number('Progress', 50, {
+        max: 100,
+        min: 0,
+        range: true,
+        step: 1,
+      })} />
     </MockWrapper>
   ))
