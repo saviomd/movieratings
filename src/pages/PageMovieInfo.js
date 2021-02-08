56b3739f9cf88b25b1eb1fce80ffbb53a908cdcf
Sávio Mendes <saviomd@users.noreply.github.com>
2021-02-07 22:47:31 -0300

    Update diary, ratings, dependencies; Update PageMovieInfo to also use diary data

--- src/pages/PageMovieInfo.js
+++ src/pages/PageMovieInfo.js
@@ -1,20 +1,19 @@
 import React, { useContext } from 'react';
 
 import { MovieInfoStore } from '../contexts/movieInfoContext';
+import movieDiaryContext from '../contexts/movieDiaryContext';
 import movieRatingsContext from '../contexts/movieRatingsContext';
-import LoadingHandler from '../components/LoadingHandler';
 import MovieInfo from '../components/MovieInfo';
 
 const PageMovieInfo = ({ match }) => {
-	const { movieRatings, movieRatingsStatus } = useContext(movieRatingsContext);
+	const { movieDiary } = useContext(movieDiaryContext);
+	const { movieRatings } = useContext(movieRatingsContext);
 	const { movieId } = match.params;
-	const movie = movieRatings.find((obj) => (obj.Id === movieId));
+	const movie = [...movieDiary, ...movieRatings].find((obj) => (obj.Id === movieId));
 	return (
-		<LoadingHandler dataStatus={movieRatingsStatus}>
-			<MovieInfoStore movie={movie}>
-				<MovieInfo />
-			</MovieInfoStore>
-		</LoadingHandler>
+		<MovieInfoStore movie={movie}>
+			<MovieInfo />
+		</MovieInfoStore>
 	);
 };
 
