5b509f9cad6e711d79473ddb68e0d57a252a6ad7
Sávio Mendes <saviomd@users.noreply.github.com>
2022-11-03 23:56:53 -0300

    Update pwa display, dependencies

---
+++ docs/assets/PageNotFound.09f01c84.js
@@ -0,0 +1,1 @@
+import{a,h as e}from"./index.f214caff.js";function t(){return a(e,{type:"pageNotFound"})}export{t as default};
--- docs/assets/PageNotFound.ef05ef20.js
+++
@@ -1,1 +0,0 @@
-import{a,h as e}from"./index.150590bc.js";function t(){return a(e,{type:"pageNotFound"})}export{t as default};
