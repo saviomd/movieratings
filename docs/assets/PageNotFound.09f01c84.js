6d3f0944d8a0a84a7cc4a3416d58780fb2b2d863
Sávio Mendes <saviomd@users.noreply.github.com>
2022-12-08 12:03:04 -0300

    Update diary, ratings, dependencies

--- docs/assets/PageNotFound.09f01c84.js
+++
@@ -1,1 +0,0 @@
-import{a,h as e}from"./index.f214caff.js";function t(){return a(e,{type:"pageNotFound"})}export{t as default};
---
+++ docs/assets/PageNotFound.ee15d8ea.js
@@ -0,0 +1,1 @@
+import{a,h as e}from"./index.545c2da6.js";function t(){return a(e,{type:"pageNotFound"})}export{t as default};
