4086f1938f5b3bce75222bf3c77a4ec8817eb125
Sávio Mendes <saviomd@users.noreply.github.com>
2023-04-27 00:54:27 -0300

    Add gha workflow; Update context filenames; Remove docs folder

--- docs/assets/ErrorRoute-8fb6ac5a.js
+++
@@ -1,1 +0,0 @@
-import{u as r,j as s,F as t,a,M as o}from"./index-e94f58fd.js";function n(){const e=r();return s(t,{children:[a(o,{type:e.statusText==="Not Found"?"pageNotFound":"error"}),s("div",{className:"small text-center",children:["(",e.statusText||e.message,")"]})]})}export{n as default};
